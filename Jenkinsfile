a8e61a53cd682d3b27694ea6277b6157f3444a7a
Radhika <radhika.kondavaram0@walmart.com>
2023-11-19 22:43:07 +0530

    added Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,35 @@
+pipeline{
+    agent any
+    tools{
+        maven 'MAVEN_HOME'
+    }
+    stages{
+        stage('Clone code from Git'){
+            steps{
+            checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/walmart-2023-sep-ascend/Team06-Shopping-Feedback-Service.git']])
+            }
+        }
+        stage('Maven package'){
+            steps{
+            bat 'mvn clean install'
+            }
+        }
+        stage('Build Docker image'){
+            steps{
+                script{
+                    bat 'docker build -t shopping-feedback-docker.jar .'
+                }
+            }
+        }
+        //stage('Push image to Docker hub'){
+        //    steps{
+        //        script{
+        //            withCredentials([string(credentialsId: 'dockerpwd', variable: 'dockerpassword')]) {
+        //            bat 'docker login -u radhika17 -p ${dockerpassword}'
+        //            bat 'docker push inventory-update-docker.jar'
+        //}
+               // }
+        //    }
+        //}
+    }
+}
--- src/test/java/com/capstone/ShoppingFeedback/ShoppingFeedbackApplicationTests.java
+++ src/test/java/com/capstone/ShoppingFeedback/ShoppingFeedbackApplicationTests.java
@@ -1,49 +1,13 @@
 package com.capstone.ShoppingFeedback;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import com.capstone.ShoppingFeedback.model.FeedbackResponse;
-import com.capstone.ShoppingFeedback.model.Response;
-
-import java.util.List;
-import java.util.ArrayList;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class ShoppingFeedbackApplicationTests {
-	
-	
-	private MockMvc mockMvc;
-	
-	@Autowired
-	private WebApplicationContext context;
-	
-	@Before
-	private void setup() {
-		mockMvc=MockMvcBuilders.webAppContextSetup(context).build();
-	}
-	
+
 	@Test
-	public void addResponseTest() {
-		List<Response> responselist= new ArrayList<Response>();
-		//responselist.add(1, null);
-		//Response responselist=new Response();
-		//responselist.add(5);
-		FeedbackResponse response=new FeedbackResponse();
-		response.setCartId(101);
-		response.setUserId(1234);
-		//response.setResponse(new List<Response> responselist);
-		//response.setResponse([4,5,6,7,2]);
-		response.setAverageRating(4.5f);
-		response.setComments("Good experience");
+	void contextLoads() {
 	}
 
 }
